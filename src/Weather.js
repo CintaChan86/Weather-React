a516e276552fc3719c34d756aaa9e032873e5b5c
CintaChan86 <80836951+CintaChan86@users.noreply.github.com>
2021-06-16 15:23:00 +0200

    Added Github

--- src/Weather.js
+++ src/Weather.js
@@ -65,4 +65,7 @@ export default function Weather(props) {
       </div>
     );
   }
+  <small>
+                <a href="https://github.com/CintaChan86/Weather-React"> Coded </a> by Yacinta Hostiningtyas
+  </small>
 }
